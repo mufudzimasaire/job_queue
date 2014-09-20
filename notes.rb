a32cee1b2b638183067740bac847bd041150e68c
Mufudzi Masaire <mufudzimasaire@gmail.com>
2014-09-20 16:52:07 +0100

    Refactored the job ordering code. Also now identifying circular dependencies based on the similar_jobs count. If similar jobs count is greater than 1, this indicates a circular dependency. As such, an exception is raised

--- notes.rb
+++ notes.rb
@@ -1,7 +1,7 @@
 =begin
-  Investigating the problem using irb.
-  This approach generally enables me to get my head around /or better understand the nature of the problem.
-  As a result I am able to come up with possible ideas on how to best solve the problem.
+	Investigating the problem using irb.
+	This approach generally enables me to get my head around /or better understand the nature of the problem.
+	As a result I am able to come up with possible ideas on how to best solve the problem.
 =end
 
 
@@ -22,9 +22,6 @@ c = Array.new
 b.map { |element| element.length.eql?(1) ? c << element.split : c << element.split(/ /) }
 => [["a"], ["b", "c"], ["c", "f"], ["d", "a"], ["e", "b"], ["f"]]
 
-# Identify circular dependencies at this point
-#raise ArgumentError.new("Jobs can’t have circular dependencies.")
-
 # ordering dependencies
 jobs = Array.new
 temp = []
@@ -39,22 +36,28 @@ c.map { |element| element.count > 1 ? jobs << (temp[0], temp[1] = element[1], el
 
 ordered_jobs = []
 jobs.map do |job|
-	if ordered_jobs.empty? || (ordered_jobs & job).empty?
+	if ordered_jobs.empty? || ( ordered_jobs & job ).empty?
+		#Compare 2 arrays for matches
 		ordered_jobs << job
-	end
-	unless (ordered_jobs & job).empty?
 		ordered_jobs.flatten!
+	else
 		similar_job = ordered_jobs & job
-		position = ordered_jobs.index(similar_job.join).to_i
-		ordered_jobs[position] = job
+		# Identify circular dependency
+		# if similar_job is greater than 1,
+		# this indicates circular dependency
+		if similar_job.count.eql?(1)
+			position = ordered_jobs.index(similar_job.join).to_i
+			ordered_jobs[position] = job
+			ordered_jobs.flatten!
+		else
+			raise ArgumentError.new("Jobs can’t have circular dependencies.")
+		end
 	end
-	ordered_jobs.flatten!
+	ordered_jobs
 end
 
 => ["a", "d", "f", "c", "b", "e"]
-# matches expected result
-
-
+# Matches expected result
 
 
 
