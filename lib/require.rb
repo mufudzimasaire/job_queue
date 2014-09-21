7aaaabba6b5356563b1ac0e0b4edd7a57ce69a79
Mufudzi Masaire <mufudzimasaire@gmail.com>
2014-09-21 10:16:17 +0100

    Require file - loads all lib files

---
+++ lib/require.rb
@@ -0,0 +1,10 @@
+=begin
+	Author: Mufudzi Masaire
+	Require file - loads all lib files
+=end
+
+require './lib/queue'
+require './lib/parser'
+require './lib/order'
+require './lib/dependency_validator'
+require './lib/exception_handler'
